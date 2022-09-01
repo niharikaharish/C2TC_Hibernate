c42b7497a3ba7aae6b335071b48ec8b35cb9b494
Niharika Harish <Niharika Harish@DESKTOP-9JACVNH.bbrouter>
2022-09-01 14:15:32 +0530

    First Commit

---
+++ JPAClassPerTableInheritance/src/com/tns/jpastart/entities/Manager.java
@@ -0,0 +1,21 @@
+package com.tns.jpastart.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="mgr_store2")
+public class Manager extends Employee {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String departmentName;
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+}

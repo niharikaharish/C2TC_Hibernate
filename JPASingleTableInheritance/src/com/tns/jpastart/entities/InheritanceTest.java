6172ba6a22a8d7d66907d3162f20b29d25fb2f23
Niharika Harish <Niharika Harish@DESKTOP-9JACVNH.bbrouter>
2022-09-01 14:17:24 +0530

    First Commit

---
+++ JPASingleTableInheritance/src/com/tns/jpastart/entities/InheritanceTest.java
@@ -0,0 +1,32 @@
+package com.tns.jpastart.entities;
+import java.util.Scanner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class InheritanceTest {
+public static void main(String[] args) {
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em = factory.createEntityManager();
+		
+		em.getTransaction().begin();
+		/*
+		 * Employee emp1=new Employee(); emp1.setName("Gayatri"); emp1.setSalary(10000);
+		 * 
+		 * em.persist(emp1);
+		 */
+		
+		
+		Manager manager1=new Manager();
+		manager1.setName("Vikas");
+		manager1.setSalary(100000);
+		manager1.setDepartmentName("C2TC");
+		em.persist(manager1);
+		em.getTransaction().commit();
+		System.out.println("Added one employee and manager to database.");
+		em.close();
+		factory.close();
+	}
+}
